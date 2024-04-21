2973ffe139bc360e95e6e56fd7d7f58aa9d7865b
ikukee <rmpanis2@gmail.com>
2024-04-21 15:26:15 +0800

    Update app.js

--- server-side/app.js
+++ server-side/app.js
@@ -4,9 +4,6 @@ import privateAPI from "./routes/auth.js";
 import cors from "cors";
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("TEST")
-})
 app.use("/api/", publicAPI);
 app.use("/api/", privateAPI)
 app.listen(3000, () => {
