9e2e458b59e9fd9d9ef95ee3a5af05450a264d33
ikukee <rmpanis2@gmail.com>
2024-04-21 16:42:02 +0800

    added some

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {BrowserRouter, Routes,Route, Navigate} from "react-router-dom"
 import { Landing } from './views/Landing';
 import { Navbar } from './components/Navbar';
+import { Project } from './views/Project';
+import { ProjectList } from './views/ProjectList';
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,12 @@ function App() {
         <div className='pages'>
           <Routes>
             <Route path='/' element={<Landing/>}/>
+          </Routes>
+          <Routes>
+            <Route path='/projects' element={<ProjectList/>}/>
+          </Routes>
+          <Routes>
+            <Route path='/projects/:id' element={<Project/>}/>
           </Routes>
         </div>
       </BrowserRouter>
---
+++ client-side/src/views/Project.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Project = ({id}) => {
+    console.log(id)
+  return (
+    <div>Project</div>
+  )
+}
